9822bc0342233e21a7c67de45476197d6f958260
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-04-07 12:58:40 -0400

    code, need to debug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+/**
+* Do basic math.
+*/
+function doMathClicked() {
+    // input
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+
+
+    // process
+    const celsius = (fahrenheit - 32) * 5 / 9
+
+
+    // output
+    document.getElementById("celsius").innerHTML =
+        "<p>The celsius is: " + celsius.toFixed(2) + " °. </p>"
+}
